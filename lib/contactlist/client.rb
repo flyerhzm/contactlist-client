8c24ab291058e6ed08616b4c06e3bad42907e2a4
Richard <richard@ekohe.com>
2009-10-30 16:17:31 +0800

    connect remote service

--- lib/contactlist/client.rb
+++ lib/contactlist/client.rb
@@ -7,13 +7,13 @@ require 'json'
 module ContactList
   class Client
 
-    # REQUEST_URI = 'http://contacts.huangzhimin.com'
-    REQUEST_URI = 'http://localhost:8080/ContactListService/contacts'
+    REQUEST_URI = 'http://contacts.huangzhimin.com/'
+    #REQUEST_URI = 'http://localhost:8080/ContactListService/contacts'
 
     Contact = Struct.new(:username, :email)
 
     def self.fetch(account, password, type)
-      response = Net::HTTP.post_form URI.parse(REQUEST_URI), {'account' => account, 'password' => password, 'type' => type}
+      response = get_response(REQUEST_URI, {'account' => account, 'password' => password, 'type' => type})
       contacts = []
       data = JSON.parse(response.body)
       data['contacts'].each do |contact|
@@ -21,6 +21,15 @@ module ContactList
       end
       contacts
     end
+    
+    private
+      def self.get_response(url, post_data)
+        response = Net::HTTP.post_form URI.parse(url), post_data
+        if Net::HTTPRedirection === response
+          response = get_response(response['location'], post_data)
+        end
+        response
+      end
   end
 end
 
